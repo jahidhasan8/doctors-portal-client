638051dce3c843414113bbea66e825bb11447949
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-12 19:28:38 +0600

    services section modified

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -43,10 +43,10 @@ const Services = () => {
                 }
             </div>
 
-            <div className="hero text-slate-600 w-2/3 mt-24 mx-auto">
+            <div className="hero text-slate-600  mt-24 mx-auto">
                 <div className="hero-content flex-col  lg:flex-row p-0">
-                    <img src={treatment} className="w-1/2 rounded-lg shadow-2xl" alt='' />
-                    <div className='w-1/2'>
+                    <img src={treatment} className="rounded-lg shadow-2xl hidden md:block md:w-[400px]" alt='' />
+                    <div className="md:w-[400px] px-6">
                         <h1 className="text-3xl text-center font-bold">Exceptional Dental Care, on Your Terms</h1>
                         <p className="py-6 px-4">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                         <button className="btn btn-primary mx-4 text-white">Get Started</button>
