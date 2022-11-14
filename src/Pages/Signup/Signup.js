4987dbaaba4449e540d9aea01056cc2971e42fec
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-14 16:09:29 +0600

    password validation added on signup form

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -41,7 +41,8 @@ const Signup = () => {
                         </label>
                         <input type="password" {...register('password',{
                             required:"Password is required",
-                            minLength:{value:6 , message:'Password must be 6 characters or long'}
+                            minLength:{value:6 , message:'Password must be 6 characters or long'},
+                            pattern:{value:/(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message:'Password must be strong'}
                         })}  className="input input-bordered w-full max-w-xs" />
                         {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
                     </div>
