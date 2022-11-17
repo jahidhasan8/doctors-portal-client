71dc3bcf84570b07db1ba3d8833a2907c608ce02
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-17 19:37:15 +0600

    dashboard modified

--- src/Pages/Dashboard/AllUsers/AllUsers.js
+++ src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllUsers = () => {
-    const { data: users = [] } = useQuery({
+    const { data: users = [],refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/users')
@@ -10,6 +11,23 @@ const AllUsers = () => {
             return data;
         }
     })
+
+    const handleMakeAdmin=(id)=>{
+        fetch(`http://localhost:5000/users/admin/${id}`,{
+            method:'PUT',
+            headers:{
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log((data));
+            if(data.modifiedCount){
+                toast.success('Make Admin Successful')
+                refetch()
+            }
+        })
+    }
     return (
         <div>
             <h2 className="text-3xl">All Users</h2>
@@ -22,8 +40,8 @@ const AllUsers = () => {
                             <th></th>
                             <th>Name</th>
                             <th>Email</th>
-                            <th>Favorite Color</th>
-                            <th>Favorite Color</th>
+                            <th>Admin</th>
+                            <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -33,8 +51,12 @@ const AllUsers = () => {
                                 <th>{i+1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td>Blue</td>
-                                <td>Blue</td>
+                                <td>{
+                                    user?.role !=='admin' &&
+                                    <button onClick={()=>handleMakeAdmin(user._id)} className='btn btn-xs btn-primary'>Make Admin</button>
+                                    
+                                    }</td>
+                                <td><button className='btn btn-xs text-red-600 bg-white font-bold'>Delete</button></td>
                             </tr>)
                         }
 
