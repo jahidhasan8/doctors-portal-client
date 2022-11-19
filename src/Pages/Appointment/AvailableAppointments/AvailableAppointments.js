62aadd14f3a60165de8c6e3f2ec959802858b95f
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-20 02:13:29 +0600

    appointmentcollection modified

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -5,21 +5,21 @@ import Loading from '../../Shared/Loading/Loading';
 import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOption from './AppointmentOption';
 
-const AvailableAppointments = ({selectedDate}) => {
+const AvailableAppointments = ({ selectedDate }) => {
     // const [appointmentOptions, setAppointmentOptions]=useState([])
-     const date=format(selectedDate,'PP')
-    const[treatment, setTreatment]=useState(null)
-    const {data:appointmentOptions=[],refetch,isLoading}=useQuery({
-        queryKey:["appointmentOptions", date],
-        queryFn: async()=>{
+    const date = format(selectedDate, 'PP')
+    const [treatment, setTreatment] = useState(null)
+    const { data: appointmentOptions = [], refetch, isLoading } = useQuery({
+        queryKey: ["appointmentOptions", date],
+        queryFn: async () => {
             // const res=await fetch(`http://localhost:5000/appointmentOptions?date=${date}`)
-            const res=await fetch(`http://localhost:5000/v2/appointmentOptions?date=${date}`)
-          const data=await  res.json()
-          return data
+            const res = await fetch(`http://localhost:5000/v2/appointmentOptions?date=${date}`)
+            const data = await res.json()
+            return data
         }
     })
 
-    if(isLoading){
+    if (isLoading) {
         return <Loading></Loading>
     }
 
@@ -30,25 +30,25 @@ const AvailableAppointments = ({selectedDate}) => {
     // },[])
     return (
         <section className='my-16'>
-            <p className='text-center text-secondary font-bold'>Available Appointments on {format(selectedDate,'PP')}</p>
+            <p className='text-center text-secondary font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6'>
-               {
-                appointmentOptions.map(option=><AppointmentOption
-                key={option._id}
-                appointmentOption={option}
-                setTreatment={setTreatment}
-                ></AppointmentOption>)
-               }
+                {
+                    appointmentOptions.map(option => <AppointmentOption
+                        key={option._id}
+                        appointmentOption={option}
+                        setTreatment={setTreatment}
+                    ></AppointmentOption>)
+                }
             </div>
-           { 
-            treatment &&
-           <BookingModal
-            treatment={treatment}
-            selectedDate={selectedDate}
-            setTreatment={setTreatment}
-            refetch={refetch}
-            ></BookingModal>}
+            {
+                treatment &&
+                <BookingModal
+                    treatment={treatment}
+                    selectedDate={selectedDate}
+                    setTreatment={setTreatment}
+                    refetch={refetch}
+                ></BookingModal>}
         </section>
     );
 };
