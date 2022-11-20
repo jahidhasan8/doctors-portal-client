156f6a3aa761f9ddbb60b7339df7f0fcd1999a1f
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-20 18:49:37 +0600

    payment gateway part modified,checkout page added

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -11,13 +11,15 @@ const Payment = () => {
     const booking = useLoaderData()
     const { treatment, price, appointmentDate, slot } = booking
     return (
-        <div>
+        <div className='text-slate-500'>
             <h3 className="text-3xl">Payment for {treatment}</h3>
             <p className='text-xl'>Please pay <strong>${price}</strong> for your appointment on {appointmentDate} at{slot}</p>
 
             <div className='w-96 my-12'>
                 <Elements stripe={stripePromise}>
-                    <CheckoutForm />
+                    <CheckoutForm 
+                    booking={booking}
+                    />
                 </Elements>
             </div>
         </div>
---
+++ src/Pages/Dashboard/Payment/_payment-inegration-steps.js
@@ -0,0 +1,13 @@
+
+
+/* 
+Collect card information
+1. install stripe
+2. card loadStripe
+: pk, publish key
+3. card element 
+4. card form 
+5. stripe, elements
+6. check card error and display error
+
+*/
